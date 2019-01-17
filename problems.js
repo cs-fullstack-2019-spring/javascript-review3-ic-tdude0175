e32a878311cb866c1017010baba56b7b215c87fe
tdude0175 <tdude0175@gmail.com>
2019-01-17 10:44:25 -0600

    put asside for graded work

---
+++ problems.js
@@ -0,0 +1,36 @@
+//Put the start of your program in a main function.
+// Create a class for a bank account user.
+// Include name, address, and account balance.
+// Create a function that can change the account balance.
+// It WILL NOT be changed if you log it.
+
+/*class will build it with whatever name
+
+ */
+
+main();
+
+function main()
+{
+    function problem1()
+    {
+        class regions
+        {
+            constructor(name, address, accountBalance)
+            {
+                this.name = name;
+                this.address = address;
+                this.accountBalance = accountBalance;
+
+            }
+
+            withdraw(amount)
+            {
+                return this.accountBalance -= amount;
+            }
+
+        }
+    }
+    var
+
+}
